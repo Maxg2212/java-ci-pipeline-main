600a5d49643ec30ef995091e409e0439793e8a3f
Max Garro Mora <maxgarro22@gmail.com>
2021-09-07 16:07:05 -0600

    [Update] Prueba

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     //ee
 
     public double sum(double a, double b) {
-        double res = a + b;
+        double res = a + b; //Myke Towers
         return res;
     }
 
